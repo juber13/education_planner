bd3b27f9dba76572d39d09eb54025ba42b551ab6
juber <juberk1010@gmail.com>
2023-12-23 21:47:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data , setData] = useState([]);
+  const [subject , setSubject] = useState("");
+  const [hourse , setHourse] = useState("");
+
+  const addSubject = () => {
+    if(subject !== "" && hourse !== ""){
+      setData([...data , {name : subject , id : data.length , hour : Number(hourse)}])
+      // setList([...lists , {name : item , id : lists.length}])
+      setSubject("");
+      setHourse("");
+    }else{
+      alert("pls add values 😐");
+      return;
+    }
+  } 
+
+
+  const increaseHousre = (id) => {
+    // const newHours = data[id].hourse + 1;
+    const newData = [...data];
+    newData[id].hour = newData[id].hour + 1;
+    setData(newData);
+  }
+  
+  const decreaseHousre = (id) => {
+    const newData = [...data];
+    if(newData[id].hour > 0){
+      newData[id].hour = newData[id].hour - 1;
+      setData(newData);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="header">
+          <h3>Geekster Education Planner</h3>
+       </div>
+
+       <div className="inputs">
+          <input value={subject} type="text" placeholder='Subject' onChange={(e) => setSubject(e.target.value)}/>
+          <input value={hourse} type="number"  placeholder='Hourse' onChange={(e) => setHourse(e.target.value)}/>
+          <button type='submit' className='btn' onClick={addSubject}>Add</button>
+       </div>
+
+       <div className="contents">
+           {data.map((item , index) => { 
+            return(
+             <div className="lists" key={index}>
+              <p>{item.name}</p>
+              <p>{item.hour}</p>
+              <button className='btn' onClick={() => increaseHousre(index)} style={{background: "yellowgreen"}}>Plus</button>
+              <button className='btn' onClick={() => decreaseHousre(index) } style={{background:"orange"}}>Minus</button>
+             </div>
+            )
+           })}
+           
+       </div>
     </div>
   );
 }
